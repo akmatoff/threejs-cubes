0fe0c87f1349eb13ecce0d3131aff46b21bf58b5
akmatoff <azim.akmatov1@gmail.com>
2020-05-02 23:42:11 +0600

    a lot of cubes added

--- index.js
+++ index.js
@@ -9,14 +9,22 @@ audioSrc.connect(analyser);
 audioSrc.connect(ctx.destination);
 var fqData = new Uint8Array(analyser.frequencyBinCount);
 
+var mouseX = 0, mouseY = 0;
+var windowHalfX = window.innerWidth / 2;
+var windowHalfY = window.innerHeight / 2;
+
 var scene = new THREE.Scene();
-scene.fog = new THREE.Fog({color: 0xCC33FF, near: 1.7, far: 1000.5});
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+scene.fog = new THREE.Fog(0x9C91FF, 5, 2550);
+scene.background = new THREE.Color(0x101015);
+
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
 var renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
-// renderer.setClearColor("#52c786");
+
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
+
+var mouseX = 0, mouseY = 0;
 
 hemiLight = new THREE.HemisphereLight(0x66dbe8, 0xcfd1d0, 4);
 scene.add(hemiLight); 
@@ -25,20 +33,46 @@ var light = new THREE.PointLight(0x72dbc2, 10, 100);
 light.position.set(0, 100, 0);
 scene.add(light);
 
-var spotlight = new THREE.SpotLight(0xc1e0e3, 9);
+var spotlight = new THREE.SpotLight(0xc1e0e3, 22);
 spotlight.castShadow = true;
 spotlight.shadow.bias = -0.0001;
 spotlight.shadow.mapSize.width = 1024*4;
 spotlight.shadow.mapSize.height = 1024*4;
 scene.add(spotlight);
 
-var sphereGeometry = new THREE.SphereGeometry(10, 100, 100);
-var sphereMaterial = new THREE.MeshLambertMaterial({color: 0x215105});
+var sphereGeometry = new THREE.SphereGeometry(5, 5, 5);
+var sphereMaterial = new THREE.MeshLambertMaterial({color: 0xFFFFFF});
 var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-sphere.castShadow = true;
-scene.add(sphere);
 
-camera.position.z = 40;
+var cubes = [];
+
+var boxGeometry = new THREE.BoxGeometry(35, 35, 35);
+var boxMaterial = new THREE.MeshLambertMaterial({color: 0xC3FEB});
+
+var boxGroup = new THREE.Group();
+
+for (let i = 0; i < 1000; i++) {
+    var cube = new THREE.Mesh(boxGeometry, boxMaterial);
+
+    cube.castShadow = true;
+    cube.receiveShadow = true;
+    cube.position.x = Math.random() * 2000 - 1000;
+    cube.position.y = Math.random() * 2000 - 1000;
+    cube.position.z = Math.random() * 2000 - 1000;
+
+    cube.rotation.x = Math.random() * Math.PI * 2;
+    cube.rotation.y = Math.random() * Math.PI * 2;
+
+    boxGroup.add(cube);
+    cubes.push(cube);
+}
+
+scene.add(boxGroup);
+boxGroup.position.y = -100;
+boxGroup.position.x = -100;
+
+camera.position.z = 2500;
+
 
 function render() {
     requestAnimationFrame(render);
@@ -51,14 +85,28 @@ function render() {
         camera.position.x + 5,
         camera.position.y + 5,
         camera.position.z + 5
-    );    
+    );   
 
     for (let fq = 0; fq < 256; fq++) {
-        sphere.position.z = fqData[fq] * 0.05;
-        spotlight.intensity = fqData[fq] * 0.05;
-        hemiLight.intensity = 4 + fqData[fq] * 0.02;
+        spotlight.intensity = fqData[fq] * 0.01;
+        cube.position.x = fqData[fq] * 0.02;
         bgContainer.style.filter = "blur(" + fqData[fq] * 0.03 + "px)";
+
     }
+
+    camera.position.z -= (mouseY - camera.position.y) * 0.001;
+    boxGroup.rotation.y += (mouseY - boxGroup.rotation.y) * 0.001 / 1000;
+    boxGroup.rotation.x += (mouseX - boxGroup.rotation.x) * 0.001 / 1000;
+
+    cubes.forEach((el) => {
+        el.rotation.x += (mouseX - el.rotation.x) * 0.01 / 1000;
+        el.rotation.y += (mouseY - el.rotation.y) * 0.01 / 1000;
+    });
+}
+
+function onMouseMove(e) {
+    mouseX = (e.clientX - windowHalfX) * 10;
+    mouseY = (e.clientY - windowHalfY) * 10;
 }
 
 function onWindowResize() {
@@ -70,6 +118,8 @@ function onWindowResize() {
 window.addEventListener('mousemove', (e) => {
     bgContainer.style.transform = `translate(-${e.clientX * 0.04 - 1}px, -${e.clientY * 0.04 - 1}px)`;
 });
+
+document.addEventListener('mousemove', onMouseMove, false);
 
 window.addEventListener('resize', onWindowResize, false);
 render();   
